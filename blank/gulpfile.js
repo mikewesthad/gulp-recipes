92e9b263a6644cb4edfe72b017e8e8fe68aea167
Michael Hadley <michaelwesthadley@gmail.com>
2016-04-13 20:04:11 -0500

    Adding a blank gulp template

---
+++ blank/gulpfile.js
@@ -0,0 +1,16 @@
+/**
+ * Blank Gulp Template
+ */
+
+
+// -- Setup --------------------------------------------------------------------
+
+// Gulp & gulp plugins
+var gulp = require("gulp");
+
+
+// -- Gulp Tasks ---------------------------------------------------------------
+
+gulp.task("default", function () {
+	console.log("I'm the default task");
+});
