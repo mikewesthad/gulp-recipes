21cdb8a2cadc3022a60e1976edb0a81db3e5bfb1
rex <retwedt@gmail.com>
2016-06-08 11:26:32 -0500

    update path to angular2 dependencies, add a reference path to avoid transpiler errors

--- 05-angular2-typescript/src/app/main.ts
+++ 05-angular2-typescript/src/app/main.ts
@@ -1,3 +1,6 @@
-import { bootstrap }    from 'angular2/platform/browser';
+/* Avoid: 'error TS2304: Cannot find name <type>' during compilation */
+///<reference path="../../typings/index.d.ts"/>
+
+import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
 bootstrap(AppComponent);
