a0b7d5d23ee60d2968ceb499b55c31b699a91d36
Michael Hadley <michaelwesthadley@gmail.com>
2016-06-12 06:44:03 -0500

    Update SASS recipe with path variables, sourcemaps and watch task

--- 02-sass/gulpfile.js
+++ 02-sass/gulpfile.js
@@ -1,47 +1,75 @@
-// SASS Compiler
-// Allow users to easily create custom builds of your libary
+/**
+ * SASS
+ *
+ * The recipe simply provides conversion of SASS or SCSS files to CSS. This
+ * could be useful to include if distributing a library that can be easily
+ * customized with SASS.
+ *
+ * The CSS is autoprefixed and has a sourcemap applied. The automatic vendor
+ * prefixes make your SASS less verbose, and the sourcemaps make it easier to
+ * inspect styles in your browser's inspector. 
+ */
 
 
-// Gulp and Plugins
-// ****************
+// -- PATHS --------------------------------------------------------------------
+ 
+// Where to look for source files
+var sassSrc = "scss/**/*.{scss,sass}";
+
+// Where to output processed files
+var cssDest = "css";
+
+// Any other paths to check when a SASS import is encountered. Here you can put
+// any SASS that you refer to that isn't in src e.g. if you "@import
+// fontawesome" and you installed fontawesome through NPM, you would do
+// something like: var sassInclude = ["node_modules/font-awesome/scss"];
+var sassInclude = [];
+
+
+// -- SETUP --------------------------------------------------------------------
 
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
+var sourcemaps = require("gulp-sourcemaps");
 
 
-// Build Tasks
-// ***********
+// -- TASKS --------------------------------------------------------------------
 
-// Convert from sass to css, add vendor prefixes
+// Convert SCSS/SASS to CSS and apply vendor prefixes & a sourcemap
 gulp.task("sass", function () {
-	// Configure a sass stream so that it logs errors properly
-	var sassStream = sass({
-		outputStyle: "expanded"
-	});
-	sassStream.on("error", sass.logError);
-
-	return gulp.src("src/scss/*.scss")
-	.pipe(sassStream)
-	.pipe(autoprefixer({
-		browsers: [
-			// https://github.com/twbs/bootstrap-sass#sass-autoprefixer
-			"Android 2.3",
-			"Android >= 4",
-			"Chrome >= 20",
-			"Firefox >= 24",
-			"Explorer >= 8",
-			"iOS >= 6",
-			"Opera >= 12",
-			"Safari >= 6"
-		],
-		cascade: true
-	}))
-	.pipe(gulp.dest("dist"))
+    return gulp.src(sassSrc)
+        .pipe(sourcemaps.init())
+            .pipe(sass({ 
+                outputStyle: "compressed",
+                includePaths: sassInclude
+            }).on("error", sass.logError))
+            .pipe(autoprefixer({
+                browsers: [
+                    // Matches bootstrap: 
+                    // https://github.com/twbs/bootstrap-sass#sass-autoprefixer
+                    "Android 2.3",
+                    "Android >= 4",
+                    "Chrome >= 20",
+                    "Firefox >= 24",
+                    "Explorer >= 8",
+                    "iOS >= 6",
+                    "Opera >= 12",
+                    "Safari >= 6"
+                    // You could also do something like:
+                    // "last 2 versions"
+                    // https://github.com/postcss/autoprefixer#options
+                ],
+                cascade: true
+            }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(cssDest));
 });
 
+// Watch for changes to the SCSS/SASS files and rerun the SASS command
+gulp.task("watch", function () {
+    gulp.watch(sassSrc, ["sass"]);
+});
 
-// Default task, build the library
-gulp.task("default", [
-	"sass"
-]);
+// Default task that runs if you don't specify a task
+gulp.task("default", ["sass", "watch"]);
